ceb2ac3f3ff875b673bed3a92d855693f3ba6ba4
AWSYS-I\user1.gaio <reynaldminguez@gmail.com>
2023-02-07 14:40:49 +0800

    Updated delete files

--- my_project/build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( Abc.aaa /home/reynald/test/googletest/googlemock/my_project/build/hello_test [==[--gtest_filter=Abc.aaa]==] --gtest_also_run_disabled_tests)
-set_tests_properties( Abc.aaa PROPERTIES WORKING_DIRECTORY /home/reynald/test/googletest/googlemock/my_project/build)
-set( hello_test_TESTS Abc.aaa)
